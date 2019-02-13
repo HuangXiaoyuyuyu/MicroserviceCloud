c73b42f9c56d15d06a3103b7e20784d5021ae5f3
Huangxiaoyuyuyu <253757635@qq.com>
2019-02-13 14:34:21 +0800

    cluster-eureka

---
+++ microservicecloud-eureka-7003/src/main/java/com/atguigu/springcloud/EurekaServer7003_App.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author OliverYu
+ * @Date 2019/2/12 15:22
+ * @Email 253757635@qq.com
+ * @Description TODO
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer7003_App {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7003_App.class,args);
+    }
+}

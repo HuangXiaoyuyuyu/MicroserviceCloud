486d573bcd44b9a1e7c3d1ad2b386404a1dd9f9e
Huangxiaoyuyuyu <253757635@qq.com>
2019-02-15 13:36:22 +0800

    Custom Ribbon LoadBalance Strategy

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/myrule/MySelfRule.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/myrule/MySelfRule.java
@@ -2,6 +2,7 @@ package com.atguigu.myrule;
 
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,7 +17,10 @@ public class MySelfRule {
 
     @Bean
     public IRule myRule() {
-        return new RandomRule();
+
+        //return new RandomRule();
+        //return new RoundRobinRule();
+        return new RoundRobinRule_Oliver();
     }
 
 }

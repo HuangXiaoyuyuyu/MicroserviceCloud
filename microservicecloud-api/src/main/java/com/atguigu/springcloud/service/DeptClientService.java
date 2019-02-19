df94d9e05e38081352b844cc5b57d7c142f17b81
Huangxiaoyuyuyu <253757635@qq.com>
2019-02-19 14:37:08 +0800

    服务降级

--- microservicecloud-api/src/main/java/com/atguigu/springcloud/service/DeptClientService.java
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/service/DeptClientService.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
-@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+//@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+@FeignClient(value = "MICROSERVICECLOUD-DEPT",fallbackFactory=DeptClientServiceFallbackFactory.class)
 public interface DeptClientService {
 
     @RequestMapping(value = "/dept/get/{id}",method = RequestMethod.GET)
---
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/service/DeptClientServiceFallbackFactory.java
@@ -0,0 +1,38 @@
+package com.atguigu.springcloud.service;
+
+import com.atguigu.springcloud.entities.Dept;
+import feign.hystrix.FallbackFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @Author OliverYu
+ * @Date 2019/2/18 16:59
+ * @Email 253757635@qq.com
+ * @Description TODO
+ */
+@Component
+public class DeptClientServiceFallbackFactory implements FallbackFactory<DeptClientService> {
+    @Override
+    public DeptClientService create(Throwable throwable) {
+        return new DeptClientService() {
+            @Override
+            public Dept get(long id) {
+                return new Dept().setDeptno(id)
+                        .setDname("该ID："+id+"没有没有对应的信息,null--@HystrixCommand")
+                        .setDb_source("no this database in MySQL");
+            }
+
+            @Override
+            public List<Dept> list() {
+                return null;
+            }
+
+            @Override
+            public boolean add(Dept dept) {
+                return false;
+            }
+        };
+    }
+}
